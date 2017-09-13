2d15b10aa6257b8cd6e5fa1439a6c71e6af730c1
Christian Eichelmann <ceichelmann@gmx.de>
2017-09-13 10:43:27 +0200

    added new output variables

--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,6 +2,14 @@ output "network_id" {
     value = "${openstack_networking_network_v2.network.id}"
 }
 
+output "router_id" {
+    value = "${openstack_networking_router_v2.router.id}"
+}
+
+output "interface_port" {
+    value = "${openstack_networking_router_interface_v2.interface.port_id}"
+}
+
 output "subnet_id" {
     value = "${openstack_networking_subnet_v2.subnet.id}"
 }
